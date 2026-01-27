0daa8be3708c3abc8dc0bc6d84aed44c4fab31ad
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2026-01-27 11:24:48 -0300

    Initial commit

---
+++ src/main/java/com/curse/spring_boot_mastery/SpringBootMasteryApplication.java
@@ -0,0 +1,14 @@
+package com.curse.spring_boot_mastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMasteryApplication.class, args);
+		System.out.println("Hello, Spring Boot Mastery!");
+	}
+
+}
---
+++ src/test/java/com/curse/spring_boot_mastery/SpringBootMasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curse.spring_boot_mastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
